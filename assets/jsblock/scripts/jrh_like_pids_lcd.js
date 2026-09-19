8c48409cd670a3a35b65e896ea2f89b09d6d84c0
sankaku789 <130286575+sankaku789@users.noreply.github.com>
2026-09-19 22:17:24 +0900

    通常色LCD PIDSに遅れ赤表示を適用

--- assets/jsblock/scripts/jrh_like_pids_lcd.js
+++ assets/jsblock/scripts/jrh_like_pids_lcd.js
@@ -2,6 +2,7 @@
 
 include(Resources.id("jsblock:scripts/jrh_pids_common.js"));
 include(Resources.id("jsblock:scripts/jrh_pids_lcd_renderer.js"));
+include(Resources.id("jsblock:scripts/jrh_pids_display_rules.js"));
 
 const jrhLcdTheme = {
     defaultBackground: 0x05051F,
