bba69e76069eb3b3631e659e3b236977b179cd89
sankaku789 <130286575+sankaku789@users.noreply.github.com>
2026-09-20 14:41:10 +0900

    コンコース通常色に2色案内スクロールを適用

--- assets/jsblock/scripts/jrh_like_pids_lcd.js
+++ assets/jsblock/scripts/jrh_like_pids_lcd.js
@@ -3,6 +3,7 @@
 include(Resources.id("jsblock:scripts/jrh_pids_common.js"));
 include(Resources.id("jsblock:scripts/jrh_pids_lcd_renderer.js"));
 include(Resources.id("jsblock:scripts/jrh_pids_display_rules.js"));
+include(Resources.id("jsblock:scripts/jrh_pids_lcd_train_info.js"));
 
 const jrhLcdTheme = {
     defaultBackground: 0x05051F,
